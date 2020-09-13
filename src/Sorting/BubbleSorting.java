de3aea502c1dadfacc2000a66d3fc3494ba88274
Vishwa Mohan <vimohan@linkedin.com>
2020-09-14 02:02:05 +0530

    12 Sep 2020

--- src/Sorting/BubbleSorting.java
+++ src/Sorting/BubbleSorting.java
@@ -1,5 +1,8 @@
 package Sorting;
 
+import java.util.Hashtable;
+
+
 /***
  *  Worst sorting algorithm - TC
  *    1. Bubble
@@ -38,17 +41,44 @@ public class BubbleSorting {
  // 1,2,3,5,4
   public void bubbleSort(int[] arr){  // TC = O(n*n)
 
-    for(int i=arr.length-1;i>=0;i--){   //[ 1,2,4,3,5]   i=4,3
-      for(int j=0 ;j<i;j++){   //j=0 --> 3
+    for(int i=arr.length-1;i>=0;i--){
+      boolean isSorted = true ;
+      for(int j=0 ;j<i;j++){
 
         if(arr[j]>arr[j+1]){
+          isSorted = false ;
           int t = arr[j];
           arr[j]= arr[j+1];
           arr[j+1]=t ;
         }
       }
+      if(isSorted){
+        return;
+      }
     }
 
+  }
+
+
+  public void selectionSort(int[] arr){
+
+    Hashtable ht = new Hashtable();
+
+    int n = arr.length ;
+
+    for(int i=0;i<n;i++){
+      int smallest  = i ; // assumed that smallest element is at ith
+
+      for(int j=i+1;j<n;j++){
+        if(arr[j] < arr[smallest]){  // o(n*n)
+          smallest = j ;
+        }
+      }
+      //swap  -- o(n)
+      int temp = arr[i];
+      arr[i] = arr[smallest];
+      arr[smallest]=temp;
+    }
   }
 
   /**
